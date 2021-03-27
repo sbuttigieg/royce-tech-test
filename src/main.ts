58d3cd92c0d02b17208b97d1a2b434ebee1e5b57
sbuttigieg <s.buttigieg@united-remote.com>
2021-03-27 14:47:39 +0100

    Disable logger for ENV that is not dev

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = 3000;
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.ENV === 'dev'
+        ? ['debug', 'log', 'verbose', 'warn', 'error']
+        : false,
+  });
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
